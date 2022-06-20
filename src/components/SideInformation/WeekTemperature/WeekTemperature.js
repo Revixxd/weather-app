5e126a7a58a0e0f476a02c7494092fea9468abc5
Adam Myślicki <revixxdm@gmail.com>
2022-06-21 00:47:29 +0200

    adding basic css for weekTemperature component

--- src/components/SideInformation/WeekTemperature/WeekTemperature.js
+++ src/components/SideInformation/WeekTemperature/WeekTemperature.js
@@ -5,7 +5,25 @@ import {WeekTemperatureStyled} from "./weekTemperatureStyling"
 function WeekTemperature(){
     return(
         <WeekTemperatureStyled>
-            <h1>Test_weektemperetur</h1>
+            <h3>Today’s Hightlights </h3>
+            <div className="weekTemperature--container">
+                <div className="weekTemperature__container--element">
+                    <h2>Wind Status</h2>
+                    <h2>7mph</h2>
+                </div>
+                <div className="weekTemperature__container--element">
+                    <h2>Wind Status</h2>
+                    <h2>7mph</h2>
+                </div>
+                <div className="weekTemperature__container--element">
+                    <h2>Visibility</h2>
+                    <h2>6,4 miles</h2>
+                </div>
+                <div className="weekTemperature__container--element">
+                    <h2>Visibility</h2>
+                    <h2>6,4 miles</h2>
+                </div>
+            </div>
         </WeekTemperatureStyled>
     )
 }
--- src/components/SideInformation/WeekTemperature/weekTemperatureStyling.js
+++ src/components/SideInformation/WeekTemperature/weekTemperatureStyling.js
@@ -1,5 +1,17 @@
 import styled from "styled-components";
 
 export const WeekTemperatureStyled = styled.div`
-
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    .weekTemperature--container{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        gap: 10rem;
+    }
+    .weekTemperature__container--element{
+        width: 200px;
+        background-color: ${props => props.theme.elementBackGround};
+    }
 `
