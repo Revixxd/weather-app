c384351adf7a40c0fe511ab3bcaeae48e7fc1ca6
Adam Myślicki <revixxdm@gmail.com>
2022-06-20 18:13:50 +0200

    basic css SearchOverlay

--- src/components/SearchOverlay/SearchOverlay.js
+++ src/components/SearchOverlay/SearchOverlay.js
@@ -5,7 +5,15 @@ import { StyledSearchOverlay } from "./searchOverlayStyling";
 function SearchOverlay(){
     return(
         <StyledSearchOverlay>
-            <h1>Test</h1>
+            <button>Exit</button>
+            <div className="StyledSearchOverlay--container">
+                <div className="StyledSearchOverlay__container--searchDiv">
+                    <form>
+                        <input placeHolder="search location"></input>
+                        <button>Search</button>
+                    </form>
+                </div>
+            </div>
         </StyledSearchOverlay>
     )
 }
--- src/components/SearchOverlay/searchOverlayStyling.js
+++ src/components/SearchOverlay/searchOverlayStyling.js
@@ -7,5 +7,15 @@ export const StyledSearchOverlay= styled.div`
     width: 100%;
     height: 100%;
 
+    display: flex;
+    justify-content: center;
+    align-items: center;
     
+    background-color: #1E213A;
+
+    .StyledSearchOverlay--container{
+        height: 80%;
+        width: 80%;
+        max-width: 400px;
+    }
 `
