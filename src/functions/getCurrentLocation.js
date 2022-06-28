dd5f465360e9e68424b3162b499397d1b768dd91
Adam Myślicki <revixxdm@gmail.com>
2022-06-28 14:37:55 +0200

    Create getCurrentLocation.js
    function that import from browser Api current location

---
+++ src/functions/getCurrentLocation.js
@@ -0,0 +1,30 @@
+export function locate() {
+    const coord = new Array()
+    const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+
+    function success(pos) {
+        const crd = pos.coords;
+        
+        coord.push(crd.latitude)
+        coord.push(crd.longitude)
+        // console.log('Your current position is:');
+        // console.log(`Latitude : ${crd.latitude}`);
+        // console.log(`Longitude: ${crd.longitude}`);
+        // console.log(`More or less ${crd.accuracy} meters.`);
+        // console.log(coord)
+        
+    }
+
+    function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+        return "error"
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+    return coord
+
+}
