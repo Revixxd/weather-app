4fdaebdfd113b59487464fe9423caa502bd21294
Adam Myślicki <revixxdm@gmail.com>
2022-10-26 21:03:20 +0200

    making form edit

--- src/App.js
+++ src/App.js
@@ -1,138 +1,136 @@
-import React from "react";
-import axios from 'axios';
+import React from 'react'
+import axios from 'axios'
 
 //global styles
-import { GlobalStyles } from "./components/styling/globalStyles";
-import { StyledApp } from "./appstyling";
+import { GlobalStyles } from './components/styling/globalStyles'
+import { StyledApp } from './appstyling'
 //themes
-import { ThemeProvider } from "styled-components";
-import {lightTheme, darkTheme} from "./components/styling/themes"
+import { ThemeProvider } from 'styled-components'
+import { lightTheme, darkTheme } from './components/styling/themes'
 //components
-import SideInformation from "./components/SideInformation/SideInformation";
-import MainInformation from "./components/MainInformation/MainInformation"
+import SideInformation from './components/SideInformation/SideInformation'
+import MainInformation from './components/MainInformation/MainInformation'
 
 //function
-import { getForcastDays } from "./functions/getForcastDays";
-import { getCurrentLocation } from "./functions/getCurrentLocation";
+import { getForcastDays } from './functions/getForcastDays'
+import { getCurrentLocation } from './functions/getCurrentLocation'
 
-//data for location 
-import { cityUrl, coordUrl } from "./functions/url";
+//data for location
+import { cityUrl, coordUrl } from './functions/url'
 
 function App() {
+    //darkMode functions
+    const [theme, setTheme] = React.useState('dark')
 
-  //darkMode functions 
-  const[theme, setTheme] = React.useState("dark")
-  
-  function changeTheme(){
-    theme === "light" ? setTheme("dark") : setTheme("light")
-  }
-
-  const [data, setData] = React.useState({})
-  const [cityInfo, setCityInfo] = React.useState({})
-  const [daysForcast, setDaysForcast] = React.useState([])
-  const [todayForcast, setTodayForcast] = React.useState({})
-
-  const [searchCity, setSearchCity] = React.useState("warsaw")
-  
-  const [urlState, setUrlState] = React.useState(cityUrl)
-  
-  const [errorSearch, setErrorSearch] = React.useState(false)
-
-  const [favCity, setFavCity] = React.useState([])
-
-  React.useEffect(()=>{
-      if (localStorage.getItem("localfavCity") === ""){
-        return
-      }else{
-        let tempArray = JSON.parse(localStorage.getItem("localfavCity"))
-        setFavCity(new Set(tempArray))
-      }
-      
-  }, [])
-
-
-  React.useEffect(()=>{
-    setCityInfo(data.city)
-    if(data.list !== undefined){
-      setTodayForcast(data.list[0])
-      setDaysForcast(getForcastDays(data.list, todayForcast))
+    function changeTheme() {
+        theme === 'light' ? setTheme('dark') : setTheme('light')
     }
-    // setSearchCity('')
-  }, [data, todayForcast])
 
-  //temperature definition
-  const [degreInfo, setDegreInfo] = React.useState("celcius")
+    const [data, setData] = React.useState({})
+    const [cityInfo, setCityInfo] = React.useState({})
+    const [daysForcast, setDaysForcast] = React.useState([])
+    const [todayForcast, setTodayForcast] = React.useState({})
 
-  // for location 
-  const [cords, setCoord] = React.useState([])
+    const [searchCity, setSearchCity] = React.useState('warsaw')
 
-    function getCoords(){
+    const [urlState, setUrlState] = React.useState(cityUrl)
+
+    const [errorSearch, setErrorSearch] = React.useState(false)
+
+    const [favCity, setFavCity] = React.useState([])
+
+    React.useEffect(() => {
+        if (localStorage.getItem('localfavCity') === '') {
+            return
+        } else {
+            let tempArray = JSON.parse(localStorage.getItem('localfavCity'))
+            setFavCity(new Set(tempArray))
+        }
+    }, [])
+
+    React.useEffect(() => {
+        setCityInfo(data.city)
+        if (data.list !== undefined) {
+            setTodayForcast(data.list[0])
+            setDaysForcast(getForcastDays(data.list, todayForcast))
+        }
+        // setSearchCity('')
+    }, [data, todayForcast])
+
+    //temperature definition
+    const [degreInfo, setDegreInfo] = React.useState('celcius')
+
+    // for location
+    const [cords, setCoord] = React.useState([])
+
+    function getCoords() {
         setCoord(getCurrentLocation())
         setUrlState(coordUrl(cords))
     }
 
-    React.useEffect((searchCity)=>{
-      navigator.geolocation.getCurrentPosition(function(position) {
-        const crd = position.coords;
-        const coord = []
-        coord.push(crd.latitude)
-        coord.push(crd.longitude)
-        setCoord(coord)
-        setUrlState(coordUrl(coord))
-      }, function() {
-        setUrlState(cityUrl(searchCity))
-      });
+    React.useEffect((searchCity) => {
+        navigator.geolocation.getCurrentPosition(
+            function (position) {
+                const crd = position.coords
+                const coord = []
+                coord.push(crd.latitude)
+                coord.push(crd.longitude)
+                setCoord(coord)
+                setUrlState(coordUrl(coord))
+            },
+            function () {
+                setUrlState(cityUrl(searchCity))
+            }
+        )
     }, [])
 
-    React.useEffect(()=>{
-      axios
-      .get(urlState)
-      .then((response) => {
-        setData(response.data)
-      })
-      .catch(err => {
-        if (err.response) {
-          // console.log(err.response.status);
-          if(err.response.status === 404){
-            console.log("ERROR")
-            setErrorSearch(true)
-          }else{
-            setErrorSearch(false)
-            
-          }
-          // console.log(err.response.statusText);
-          // console.log(err.message);
-          // console.log(err.response.headers); 
-          // console.log(err.response.data); 
-        }
-      })
+    React.useEffect(() => {
+        axios
+            .get(urlState)
+            .then((response) => {
+                setData(response.data)
+            })
+            .catch((err) => {
+                if (err.response) {
+                    // console.log(err.response.status);
+                    if (err.response.status === 404) {
+                        setErrorSearch(true)
+                    } else {
+                        setErrorSearch(false)
+                    }
+                    // console.log(err.response.statusText);
+                    // console.log(err.message);
+                    // console.log(err.response.headers);
+                    // console.log(err.response.data);
+                }
+            })
     }, [urlState])
 
-  return (
-    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-      <GlobalStyles/>
-      <StyledApp>
-          <MainInformation 
-          setFavCity = {setFavCity}
-          favCity = {favCity}
-          searchCity = {searchCity} 
-          changeCity = {setSearchCity} 
-          cityInfo = {cityInfo} 
-          todayForcast = {todayForcast}
-          errorSearch = {errorSearch}
-          degreInfo = {degreInfo}
-          setUrlState = {setUrlState}
-          getCoords ={getCoords}
-          />
-          <SideInformation 
-          degreInfo = {degreInfo}
-          setDegreInfo = {setDegreInfo}
-          todayForcast = {todayForcast} 
-          daysForcast={daysForcast}
-          />
-      </StyledApp>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+            <GlobalStyles />
+            <StyledApp>
+                <MainInformation
+                    setFavCity={setFavCity}
+                    favCity={favCity}
+                    searchCity={searchCity}
+                    changeCity={setSearchCity}
+                    cityInfo={cityInfo}
+                    todayForcast={todayForcast}
+                    errorSearch={errorSearch}
+                    degreInfo={degreInfo}
+                    setUrlState={setUrlState}
+                    getCoords={getCoords}
+                />
+                <SideInformation
+                    degreInfo={degreInfo}
+                    setDegreInfo={setDegreInfo}
+                    todayForcast={todayForcast}
+                    daysForcast={daysForcast}
+                />
+            </StyledApp>
+        </ThemeProvider>
+    )
 }
 
-export default App;
+export default App
