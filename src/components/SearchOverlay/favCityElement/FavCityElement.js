813398f39ccd2b38ff07a015e390779e8f730a70
Adam Myślicki <revixxdm@gmail.com>
2022-10-26 20:52:05 +0200

    minnor visual fix for list
    changing icon for delete element in array of list fav city

--- src/components/SearchOverlay/favCityElement/FavCityElement.js
+++ src/components/SearchOverlay/favCityElement/FavCityElement.js
@@ -1,31 +1,32 @@
-import React from "react";
+import React from 'react'
 
 //styling
-import { StyledFavCityElement } from "./favCityElementstyling";
+import { StyledFavCityElement } from './favCityElementstyling'
 
 //functions
-import { deleteTask } from "../../../functions/deleteElement";
-import { cityUrl } from "../../../functions/url";
+import { deleteTask } from '../../../functions/deleteElement'
+import { cityUrl } from '../../../functions/url'
 
 //icons
-import {AiOutlineClose} from "@react-icons/all-files/ai/AiOutlineClose"
-function FavCityElement(props){
-    function afterClick(event){
+import { RiDeleteBin5Line } from '@react-icons/all-files/ri/RiDeleteBin5Line'
+function FavCityElement(props) {
+    function afterClick(event) {
         props.setUrlState(cityUrl(event.target.innerText))
         props.changeSearchState()
     }
-    return(
+    return (
         <StyledFavCityElement>
-            <h4
-            onClick={(event) =>afterClick(event)}
-            >{props.element}</h4>
+            <h4 onClick={(event) => afterClick(event)}>{props.element}</h4>
             <button
-            onClick={() => (props.setFavCity(deleteTask(props.favCity,props.element)))}
-            ><AiOutlineClose color="white"/></button>
+                onClick={() =>
+                    props.setFavCity(deleteTask(props.favCity, props.element))
+                }
+            >
+                <RiDeleteBin5Line color="white" />
+            </button>
             {/* <button>Set to daefult</button> */}
         </StyledFavCityElement>
     )
 }
-
 
 export default FavCityElement
