5c1c9857c4b74950dce42c1cf228bb95a0902633
Adam Myślicki <revixxdm@gmail.com>
2022-11-15 19:12:38 +0100

    new api key

--- src/functions/url.js
+++ src/functions/url.js
@@ -2,11 +2,11 @@ export const cityUrl = (city) => {
     if (city === undefined) {
         return coordUrl
     }
-    return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=410e713617c25eb9e018ecafd290e053`
+    return `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=4ef69c0833663e4956a91fbe52823615`
 }
 
 export const coordUrl = (array) => {
     const lat = array[0]
     const lon = array[1]
-    return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=410e713617c25eb9e018ecafd290e053`
+    return `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=4ef69c0833663e4956a91fbe52823615`
 }
